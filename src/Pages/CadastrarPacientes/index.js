0a8df07da7231b74e05175c5290b0c8baf598d2f
Nícolas Ferreira <nicolasemanuel.f@gmail.com>
2023-11-12 18:07:54 -0300

    Atualização para uso do hook-form

--- src/Pages/CadastrarPacientes/index.js
+++ src/Pages/CadastrarPacientes/index.js
@@ -1,22 +1,16 @@
 import React from "react";
 import './style.css';
+import { useForm } from "react-hook-form";
 
 function CadastrarPacientes () {
-    const handleSubmitForm = (event) => {
-        event.preventDefault()
-        const formValues = new FormData(event.target)
-        const formObject = {
-            id:Math.floor(Math.random() * 1000)
-        }
-        formValues.forEach((value, key) => {
-            formObject[key] = value
-        })
-
+    const {register, handleSubmit, reset, formState: {errors}} = useForm();
+    const handleSubmitForm = (paciente) => {
+        paciente.id = Math.floor(Math.random() * 1000)
         const previousList = sessionStorage.getItem('pacientes') ? JSON.parse(sessionStorage.getItem('pacientes')) : []
-        sessionStorage.setItem('pacientes', JSON.stringify([...previousList, formObject]))
+        sessionStorage.setItem('pacientes', JSON.stringify([...previousList, paciente]))
 
         alert('Paciente cadastrado!')
-        event.target.reset();
+        reset();
     }
 
     return (
@@ -24,21 +18,36 @@ function CadastrarPacientes () {
             <span className = 'title'>
                 Cadastro de pacientes:
             </span>
-            <form onSubmit={ handleSubmitForm }>
+            <form onSubmit={ handleSubmit(handleSubmitForm) }>
                 <label htmlFor = 'nome'>Nome</label>
-                <input name="nome" id='nome' placeholder="Digite seu nome..." />
+                <input {...register('nome', { required: true })} id='nome' placeholder="Digite seu nome..." />
+                {errors.nome?.type === "required" && (
+                    <p role="alert">O nome é obrigatório</p>
+                )}
 
                 <label htmlFor = 'sobrenome'>Sobrenome</label>
-                <input name="sobrenome" id='sobrenome' placeholder="Digite seu sobrenome..." />
+                <input {...register('sobrenome', { required: true })} id='sobrenome' placeholder="Digite seu sobrenome..." />
+                {errors.sobrenome?.type === "required" && (
+                    <p role="alert">O Sobrenome é obrigatório</p>
+                )}
 
                 <label htmlFor = 'sexo'>Sexo</label>
-                <input name="sexo" id='sexo' placeholder="Digite seu sexo..." />
+                <input  {...register('sexo', { required: true })} id='sexo' placeholder="Digite seu sexo..." />
+                {errors.sexo?.type === "required" && (
+                    <p role="alert">O sexo é obrigatório</p>
+                )}
 
                 <label htmlFor = 'endereco'>Endereço</label>
-                <input name="endereco" id='endereco' placeholder="Digite seu endereco..." />
+                <input  {...register('endereco', { required: true })} id='endereco' placeholder="Digite seu endereco..." />
+                {errors.endereco?.type === "required" && (
+                    <p role="alert">O Endereço é obrigatório</p>
+                )}
 
                 <label htmlFor = 'telefone'>Telefone</label>
-                <input name="telefone" id='telefone' placeholder="Digite seu telefone..." />
+                <input  {...register('telefone', { required: true, setValueAs: (value)=> value.replace(/[^0-9]+/g, "") })} id='telefone' placeholder="Digite seu telefone..." />
+                {errors.telefone?.type === "required" && (
+                    <p role="alert">O telefone é obrigatório</p>
+                )}
 
                 <button className = 'remove' type="reset">Cancelar</button>
                 <button className = 'submit' type="submit">Salvar</button>
